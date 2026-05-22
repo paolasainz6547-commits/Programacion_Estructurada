0daff9dc7f977751cb9494bcfafdded12de3a8c6
paolasainz6547-commits <paola.sainz6547@alumnos.udg.mx>
2026-05-22 17:21:27 -0600

    Create promedio_y_beca.c

---
+++ Actividad 03_Condicionales/Codigo/promedio_y_beca.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+int promedio, materias;
+printf ("Ingrese su promedio: ");
+scanf ("%d", &promedio);
+printf ("Ingrese la cantidad de materias reprobadas: ");
+scanf ("%d", &materias);
+
+if ( promedio>=85 && materias==0)
+{
+printf ("Felicidades! Obtienes la beca\n " );
+}
+else {
+printf ("Lo siento, no cumples con los requerimientos\n");
+}
+return 0;
+}
