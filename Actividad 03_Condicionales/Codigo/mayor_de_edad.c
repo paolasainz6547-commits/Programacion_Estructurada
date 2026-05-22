4487b434cbc279bec1f1131235bed29d351af380
paolasainz6547-commits <paola.sainz6547@alumnos.udg.mx>
2026-05-22 17:20:25 -0600

    Create mayor_de_edad.c

---
+++ Actividad 03_Condicionales/Codigo/mayor_de_edad.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+int edad;
+printf ("Ingresa tu edad : ");
+scanf ("%d", &edad);
+if (edad >=18){
+printf ("Eres mayor de edad\n ");
+}
+else {
+printf ("Eres menor de edad\n ");
+}
+return 0;
+}
