2f267c38d1cb57f7fa02cef9c462754683f965a9
paolasainz6547-commits <paola.sainz6547@alumnos.udg.mx>
2026-05-22 08:14:55 -0600

    Add files via upload

---
+++ Actividad 01/Codigo/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+ int i;
+ int N=3;
+ int M=3;
+int tablero [N][M];
+tablero [0][0]=100;
+tablero [0][1]=101;
+tablero [0][2]=102;
+tablero [1][0]=110;
+tablero [1][1]=111;
+tablero [1][2]=112;
+tablero [2][0]=120;
+tablero [2][1]=121;
+tablero [2][2]=122;
+
+
+for (int i=0; i<4; i++){
+    printf ("%d", )
+
+
+
+}
+printf ("Dato 112 esta en [1][2]:", tablero[1][2]);
+printf ("Dato 121 esta en [2][1]:", tablero[2][1]);
+
+
+
+
+
+
+
+    return 0;
+}
