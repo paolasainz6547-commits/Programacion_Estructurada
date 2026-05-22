2eab70c1eab47b0015fa79f6498cd5e91b334f40
paolasainz6547-commits <paola.sainz6547@alumnos.udg.mx>
2026-05-22 17:57:46 -0600

    Update par_impar_primo.c

--- Actividad 03_Condicionales/Codigo/par_impar_primo.c
+++ Actividad 03_Condicionales/Codigo/par_impar_primo.c
@@ -1,39 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 int main()
 {
-int A, B, C, num, i, esPrimo = 1;
-printf("Ingrese un numero: ");
-scanf ("%d", &A);
-if (A%2 ==0)
-{
-printf ("\n Su numero es par");
-} else
-printf ("No es par \n");
-printf("\n Ingrese un numero: ");
-scanf ("%d", &B);
-if (B%2==1)
-{
-printf ("Es impar\n ");
-} else
+    int A, B, C, num, i, esPrimo = 1;
 
-printf ("Es par");
+    printf("Ingrese un numero: ");
+    scanf("%d", &A);
 
-printf("\n Ingrese un numero: ");
-scanf ("%d", &C);
-printf("Ingresa un numero: ");
-scanf("%d", &num);
-if (num <= 1) {
-esPrimo = 0;
-} else {
-for (i = 2; i < num; i++) {
-if (num % i == 0) {
-esPrimo = 0;
+    if (A % 2 == 0)
+    {
+        printf("Su numero es par\n");
+    }
+    else
+    {
+        printf("No es par\n");
+    }
+
+    printf("\nIngrese un numero: ");
+    scanf("%d", &B);
+
+    if (B % 2 == 1)
+    {
+        printf("Es impar\n");
+    }
+    else
+    {
+        printf("Es par\n");
+    }
+
+    printf("\nIngresa un numero: ");
+    scanf("%d", &num);
+
+    if (num <= 1)
+    {
+        esPrimo = 0;
+    }
+    else
+    {
+        for (i = 2; i < num; i++)
+        {
+            if (num % i == 0)
+            {
+                esPrimo = 0;
+            }
+        }
+    }
+
+    if (esPrimo)
+    {
+        printf("Es un numero primo\n");
+    }
+    else
+    {
+        printf("No es un numero primo\n");
+    }
+
+    return 0;
 }
-}
-}
-if (esPrimo)
-printf("Es un numero primo\n");
-else
-printf("No es un numero primo\n");
-return 0;
+
