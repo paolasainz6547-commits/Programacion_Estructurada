1ec1457510f52ed70d04c40284636eb770326733
paolasainz6547-commits <paola.sainz6547@alumnos.udg.mx>
2026-05-22 17:59:33 -0600

    Update promedio_y_beca.c

--- Actividad 03_Condicionales/Codigo/promedio_y_beca.c
+++ Actividad 03_Condicionales/Codigo/promedio_y_beca.c
@@ -3,18 +3,20 @@
 
 int main()
 {
-int promedio, materias;
-printf ("Ingrese su promedio: ");
-scanf ("%d", &promedio);
-printf ("Ingrese la cantidad de materias reprobadas: ");
-scanf ("%d", &materias);
+    int promedio, materias;
+    printf ("Ingrese su promedio: ");
+    scanf ("%d", &promedio);
+    printf ("Ingrese la cantidad de materias reprobadas: ");
+    scanf ("%d", &materias);
 
-if ( promedio>=85 && materias==0)
-{
-printf ("Felicidades! Obtienes la beca\n " );
+    if ( promedio>=85 && materias==0)
+    {
+        printf ("Felicidades! Obtienes la beca\n " );
+    }
+    else
+    {
+        printf ("Lo siento, no cumples con los requerimientos\n");
+    }
+    return 0;
 }
-else {
-printf ("Lo siento, no cumples con los requerimientos\n");
-}
-return 0;
-}
+
