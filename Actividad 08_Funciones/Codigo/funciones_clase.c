a9cb6d35075901bd13f66e2f8dfea1034c664dab
paolasainz6547-commits <paola.sainz6547@alumnos.udg.mx>
2026-05-23 15:27:04 -0600

    Update funciones_clase.c

--- Actividad 08_Funciones/Codigo/funciones_clase.c
+++ Actividad 08_Funciones/Codigo/funciones_clase.c
@@ -1,65 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 int fsuma (int a, int b)
 {
     int S;
-    S= a+b;
+    S = a + b;
     return S;
 }
+
 void cambiar (int x)
 {
-    x=100;
+    x = 100;
 }
 
 void mostrar (int v[], int n)
 {
     int i;
-    for(i=0; i<n; i++)
+    for (i = 0; i < n; i++)
     {
-        printf ("%d", v[i]);
+        printf("%d ", v[i]);
     }
 }
 
 void mostrarMatriz (int m[2][3])
 {
-    int i,j;
-    for(i=0; i<2; i++)
+    int i, j;
+    for (i = 0; i < 2; i++)
     {
-        for (j=0; j<3; j++)
+        for (j = 0; j < 3; j++)
         {
-            printf ("%d", m[i][j]);
+            printf("%d ", m[i][j]);
         }
         printf("\n");
     }
 }
 
-void duplicar (int *x) //Funcion con pase por parametro
+void duplicar (int *x)
 {
-    *x=(*x)*2;
-
+    *x = (*x) * 2;
 }
 
 int main()
 {
-
     int resultado;
     resultado = fsuma(5, 5);
-    printf ("\n Resultado %d", resultado);
+    printf("\nResultado: %d\n", resultado);
 
     int a = 5;
     cambiar(a);
-    printf (m"%d", a);
+    printf("a = %d\n", a);
 
-    int datos[5]= {1,2,3,4,5};
+    int datos[5] = {1, 2, 3, 4, 5};
+    printf("Arreglo: ");
     mostrar(datos, 5);
+    printf("\n");
 
-    int m[2][3]= {{1,2,3},{4, 5, 6}};
+    int m[2][3] = {{1, 2, 3}, {4, 5, 6}};
+    printf("Matriz:\n");
     mostrarMatriz(m);
 
-    int n=4;
+    int n = 4;
     duplicar(&n);
-    printf ("%d", n);
+    printf("n duplicado = %d\n", n);
 
     return 0;
-
 }
+
