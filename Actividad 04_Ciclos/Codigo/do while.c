96312d413bb27880033db8913324b2b8b8e1304f
paolasainz6547-commits <paola.sainz6547@alumnos.udg.mx>
2026-05-22 18:35:13 -0600

    Update do while.c

--- Actividad 04_Ciclos/Codigo/do while.c
+++ Actividad 04_Ciclos/Codigo/do while.c
@@ -3,18 +3,27 @@
 
 int main()
 {
+    int opcion;  // Variable para almacenar la elecciÃ³n del usuario
 
-    int opcion;  //Variable para almacenar la elección del usuario
-
-    do    //Opciones del menu
+    do
     {
-        printf ("1. Continuar\n");
-        printf ("2. Salir\n");
-        scanf ("%d", &opcion);  //Leer la opcion ingresada
+        // Opciones del menu
+        printf("\n--- MENU ---\n");
+        printf("1. Continuar\n");
+        printf("2. Salir\n");
+        printf("Elige una opcion: ");
+        scanf("%d", &opcion);         // Leer la opcion ingresada
+
+        //Interfaz para el usuario
+        if (opcion == 1)
+        {
+            printf("\nElegiste continuar\n");
+        }
+
     }
-    while (opcion !=2);  //Repetir el ciclo mientras la opcion no sea 2
-    printf("Fin del menu.\n"); //Mensaje de salir del ciclo
+    while (opcion != 2);    // Repetir el ciclo mientras la opcion no sea 2
+
+    printf("\nFin del menu.\n"); // Mensaje de salir del ciclo
+
     return 0;
 }
-
-
