92af8016dc96eb7e5b2418f2a242de705044a46f
paolasainz6547-commits <paola.sainz6547@alumnos.udg.mx>
2026-05-22 18:44:16 -0600

    Update while.c

--- Actividad 04_Ciclos/Codigo/while.c
+++ Actividad 04_Ciclos/Codigo/while.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
+int main() {
     int x, suma = 0;   // Variable acumuladora inicializada en 0
 
     printf("Dame un numero (0=salir): ");
     scanf("%d", &x);
 
-    while (x != 0)  // Mientras el número no sea 0, se acumula en 'suma'
-
-    {
-        suma += x;  // Se suma el numero a la variable suma
+    // Mientras el numero no sea 0, se acumula en la variable acumuladora 'suma'
+    while (x != 0) {
+        suma += x;     // Se suma el numero a la variable suma
+        printf("Dame otro numero (0=salir): ");
         scanf("%d", &x);
     }
-    printf("La suma total es: %d\n", suma);  // Mostrar el resultado final
+
+    // Mostrar el resultado final
+    printf("\nLa suma total es: %d\n", suma);
 
     return 0;
 }
