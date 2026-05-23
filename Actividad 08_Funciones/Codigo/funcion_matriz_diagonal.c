5206194673666375394feda95defe2edd86a51f2
paolasainz6547-commits <paola.sainz6547@alumnos.udg.mx>
2026-05-23 02:57:44 -0600

    Add files via upload

---
+++ Actividad 08_Funciones/Codigo/funcion_matriz_diagonal.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void mostrar (int m[3][3])
+{
+    int i, j;
+    for (i=0; i<3; i++)
+    {
+
+        for (j=0; j<3; j++)
+        {
+            printf ("%d", m[i][j]);
+        }
+        printf ("\n");
+    }
+}
+
+void capturar (int m[3][3])  //Funcion de recorrido basico
+{
+    int i,j;
+    for (i=0; i<3; i++)
+    {
+        for (j=0; j<3; j++)
+        {
+            scanf("%d", &m[i][j]);
+        }
+        printf ("\n");
+    }
+
+}
+int diagonal (int m[3][3])
+{
+
+    int suma=0, i;
+    //suma= m[0][0]+m[1][1]+m[2][2];
+    for (i=0; i<3; i++)
+    {
+        suma+=m[i][i];
+    }
+    printf ("La suma es: %d ",suma);
+
+    return suma;
+}
+
+int main()
+{
+
+///Funciones con matrices
+
+    int m[3][3];
+    int i, j, suma;
+    printf ("\n Ingrese la matriz: ");
+    capturar(m);
+    mostrar(m);
+    suma = diagonal(m);
+
+
+    FILE*archivo;
+    archivo= fopen("tableroF.txt", "a");
+
+    if (archivo != NULL)
+    {
+        for(i=0; i<3; i++)
+        {
+            for (j=0; j<3; j++)
+            {
+
+                fprintf(archivo, "%d ",m[i][j]);
+            }
+            fprintf(archivo, "\n");
+        }
+        fprintf (archivo, "La suma es: %d", suma);
+        fclose(archivo);
+    }
+    return 0;
+
+}
